75c8f496e0007a603f0b46eeac57936cb120891c
Ahmed Makled <36972797+Ahmed-Makled@users.noreply.github.com>
2024-05-13 20:10:30 +0300

    Add Custome Plugins Upload Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,64 @@
 // app.component.ts
 
 import { Component } from '@angular/core';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { EditorConfig } from '@ckeditor/ckeditor5-core';
 import { FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import Adapter from './ckeditorAdapter';
 
-@Component( {
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-} )
+  styleUrls: ['./app.component.css']
+})
 
 export class AppComponent {
- public Editor = ClassicEditor;
-config = {
-    toolbar: {
-      items: [
-        'heading', '|',
-        'fontfamily', 'fontsize',
-        'alignment',
-        //'fontColor', 'fontBackgroundColor', '|',
-        'bold', 'italic', 'strikethrough', 'underline', 
-        //'subscript', 'superscript', '|',
-        // 'link', '|',
-        // 'outdent', 'indent', '|',
-        // 'bulletedList', '-', 'numberedList', 'todoList', '|',
-        // 'code', 'codeBlock', '|',
-        // 'insertTable', '|',
-        // 'imageUpload', 'blockQuote', '|',
-        // 'todoList'
-        // ,
-        'undo', 'redo',
-      ],
-      shouldNotGroupWhenFull: true,
-      //plugins: [ FontFamily, FontSize ],
-    },
+  public Editor = ClassicEditor;
+  htmlData: any = null
+  public componentEvents: string[] = [];
 
-    // image: {
-    //   // Configure the available styles.
-    //   styles: [
-    //     'alignLeft', 'alignCenter', 'alignRight'
-    //   ],
-
-    //   // Configure the available image resize options.
-    //   resizeOptions: [
-    //     {
-    //       name: 'resizeImage:original',
-    //       label: 'Original',
-    //       value: null
-    //     },
-    //     {
-    //       name: 'resizeImage:50',
-    //       label: '25%',
-    //       value: '25'
-    //     },
-    //     {
-    //       name: 'resizeImage:50',
-    //       label: '50%',
-    //       value: '50'
-    //     },
-    //     {
-    //       name: 'resizeImage:75',
-    //       label: '75%',
-    //       value: '75'
-    //     }
-    //   ],
-
-      // You need to configure the image toolbar, too, so it shows the new style
-      // buttons as well as the resize buttons.
-      // toolbar: [
-      //   'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
-      //   '|',
-      //   'ImageResize',
-      //   '|',
-      //   'imageTextAlternative'
-      // ]
-    //},
-    // simpleUpload: {
-    //    The URL that the images are uploaded to.
-    // uploadUrl: 'http://localhost:52536/api/Image/ImageUpload',
-
-    //   Enable the XMLHttpRequest.withCredentials property.
-
-    //},
+  config: EditorConfig = {
+    toolbar: ['heading', '|',
+      'fontfamily', 'fontsize',
+      'alignment',
+      'fontColor', 'fontBackgroundColor', '|',
+      'bold', 'italic', 'custombutton', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
+      'link', '|',
+      'outdent', 'indent', '|',
+      'bulletedList', 'numberedList', '|',
+      'code', 'codeBlock', '|',
+      'insertTable', '|',
+      'imageUpload', 'blockQuote', '|',
+      'undo', 'redo', '|',
+      'youtube',
+      'mediaEmbed'
+    ],
 
     language: 'en',
-    allowedContent: true,
+    placeholder: "Type the content here!"
+
   };
+  customAdapterPlugin(editor: { plugins: { get: (arg0: string) => { (): any; new(): any; createUploadAdapter: (loader: any) => any; }; }; config: any; }) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return new Adapter(loader, editor.config);
+    };
+  }
+  onReady(editor: { plugins: { get: (arg0: string) => { (): any; new(): any; createUploadAdapter: (loader: any) => Adapter; }; }; config: any; getData: () => any; }) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader: any) => {
+
+      return new Adapter(loader, editor.config);;
+    }; 
+  }
+
+  onFocus(event: FocusEvent): void {
+    this.componentEvents.push('Focused the editing view.');
+  }
+
+  onChange({ editor }: ChangeEvent) {
+    this.htmlData = editor?.getData();
+    console.log(this.htmlData);
+  }
+
 
 }
